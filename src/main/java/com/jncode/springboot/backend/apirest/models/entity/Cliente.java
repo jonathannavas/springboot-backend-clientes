3b0a161fee449684c2d5509e4367f9560459dbc1
Jonathan <jonathan.navas9@hotmail.com>
2021-01-10 22:25:15 -0500

    Implementacion de validaciones desde el backend api rest

--- src/main/java/com/jncode/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/jncode/springboot/backend/apirest/models/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="clientes")
@@ -21,10 +24,16 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty(message = "no puede estar vacio")
+	@Size(min = 4, max = 12,message = "el tamaño debe estar entre 4 y 12 caracteres")
 	@Column(nullable = false)
 	private String nombre;
+	
+	@NotEmpty(message = "no puede estar vacio")
 	private String apellido;
 	
+	@NotEmpty(message = "no puede estar vacio")
+	@Email(message = "no es una direccion de correo con un formato valido")
 	@Column(nullable = false,unique = true)
 	private String email;
 	
